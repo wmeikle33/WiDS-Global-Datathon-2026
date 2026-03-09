0f67df1573ad5597e5809ce4eeb26e2cdacd4c9b
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-10 00:33:55 +0800

    Delete projects directory

--- projects/WiDS/src/get_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-path = kagglehub.dataset_download("mathchi/diabetes-data-set")
-data = pd.read_csv(path + '/diabetes.csv')
-
--- projects/WiDS/src/train.py
+++
@@ -1,1 +0,0 @@
-history = model.fit(train_dataset, validation_data=eval_dataset, epochs=25, batch_size=BATCH_SIZE)
