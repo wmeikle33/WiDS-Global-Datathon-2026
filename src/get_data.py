49260b4481253014beaa9028d77bd4ea478346f6
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-15 15:57:34 +0800

    Update get_data.py

--- src/get_data.py
+++ src/get_data.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 def get_data():
   path = kagglehub.dataset_download("WiDS-Global-Datathon/diabetes-data-set")
-  data = pd.read_csv(path + '/diabetes.csv')
+  data = pd.read_csv(path + '/train.csv')
   return data
 
