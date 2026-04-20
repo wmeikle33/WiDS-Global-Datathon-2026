2f4930fad97458d2eca66ea5ee3208ed25b4cbdc
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-20 17:20:20 +0800

    Update metrics.py

--- src/datathon/metrics.py
+++ src/datathon/metrics.py
@@ -4,12 +4,10 @@ from typing import Any
 
 from sklearn.metrics import log_loss, roc_auc_score
 
-
 def WiDS_metrics(y_true, y_prob) -> dict[str, float]:
     metrics = {"log_loss": float(log_loss(y_true, y_prob))}
     metrics["auc"] = float(roc_auc_score(y_true, y_prob))
     return metrics
-
 
 def metric_score(metric_fn: Any, y_true, y_pred):
     return metric_fn(y_true, y_pred)
